a76ce4350a8a232df98fba1e403bcf99a7624197
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2024-10-25 23:25:00 +0100

    Commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,27 @@
 import express from "express"
 import morgan from 'morgan'
 import dotenv from "dotenv"
+import swaggerUI from "swagger-ui-express"
+import swaggerDocument from "../swagger.json"
 
 dotenv.config()
 
 const app = express()
 
 const PORT = process.env.DEVELOPMENT_PORT || 8080
+
+app.use(morgan('dev'))
+
+app.get('/health', (req, res) => {
+    res.send('Health Check');
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
+
+
+export default app
 
