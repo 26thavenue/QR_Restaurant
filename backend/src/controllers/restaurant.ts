1eacb5b01867da6efaba2641a59feb525dc78f60
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2024-11-05 17:34:32 +0100

    Added Middlewares

---
+++ backend/src/controllers/restaurant.ts
@@ -0,0 +1,9 @@
+import {type Request, type Response} from "express"
+
+import { RestaurantRepository } from "../repository/restaurant"
+
+const restaurantRepository = new RestaurantRepository
+
+// export const getAllRestaurants = (req:Request, res:Response) =>{
+//     const allRestaurants = 
+// }
