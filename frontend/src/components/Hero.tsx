9180d8a2ff61d694c54b5b6011b129935be73d52
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2024-11-12 07:56:49 +0100

    Made the pages

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,15 @@
+
+const Hero = () => {
+  return (
+    <div className="bg-neutral-400 w-full h-[90vh] lg:h-[80vh] flex items-center justify-center p-4">
+         <h1 className='text-4xl font-bold'>
+            Supercharge your restaurant's order, invoices and management 
+        </h1>
+        <div className='w-full h-full bg-neutral-700 rounded-md px-6'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between">
+        <div className="h-4 w-4 bg-neutral-400"></div>
+        <div>
+            <h1>Products</h1>
+            <h1>Contact Us</h1>
+            <h1>Pricing</h1>
+            <h1>Browse</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
